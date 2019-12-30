0e04b8b40621f19e338280f94d80d034ad747d68
hhh2012aa <38385178+hhh2012aa@users.noreply.github.com>
2019-12-30 15:38:48 +0800

    Update Resize_IMG.py

--- Resize_IMG.py
+++ Resize_IMG.py
@@ -1,6 +1,39 @@
 
 import json
+import boto3import json
 import boto3
+from PIL import Image
+import logging
+import tempfile
+
+
+def resize_1(input_file, key):
+    im = Image.open(input_file)
+    x_s = 640 
+    y_s = 360 
+    out = im.resize((x_s,y_s),Image.ANTIALIAS) 
+    out.save('/tmp/new_'+ key)
+    
+
+def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    s3.download_file(bucket, key, '/tmp/' + key)
+
+    resize_1('/tmp/'+ key, key)
+    logging.error(key)
+    if key[0:3] != 'Res':
+        try:
+            s3.upload_file('/tmp/new_'+ key , bucket, 'Resized-'+ key)
+        
+        except:
+            logging.error(e)
+   
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Successed")
+    }
 from PIL import Image
 import logging
 import tempfile
